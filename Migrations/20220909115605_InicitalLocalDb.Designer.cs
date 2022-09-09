29ee5d3c2f07c59d296fbec36c66ac1411aad824
Gabriel Pontes <99334150+Gabopontes@users.noreply.github.com>
2022-09-09 09:54:26 -0300

    LocalDB

--- Migrations/20220909115605_InicitalLocalDb.Designer.cs
+++ Migrations/20220909115605_InicitalLocalDb.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PcEstoque.Data;
 
@@ -11,9 +12,10 @@ using PcEstoque.Data;
 namespace PcEstoque.Migrations
 {
     [DbContext(typeof(PcEstoqueContext))]
-    partial class PcEstoqueContextModelSnapshot : ModelSnapshot
+    [Migration("20220909115605_InicitalLocalDb")]
+    partial class InicitalLocalDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
